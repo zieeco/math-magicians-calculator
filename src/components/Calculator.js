fe3a0f0a1059607934e0cc0e682832426e7359a9
zieeco <zieecosam@gmail.com>
2022-02-11 13:19:41 +0100

    Refactor using hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,52 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './Calculator.css';
 import './reset.css';
 import calculate from '../logic/calculate';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
+const Calculator = () => {
+  const [state, setState] = useState({ total: null, next: null, operation: null });
+  const { total, next, operation } = state;
 
-  executeCalculation(e) {
-    this.caluculatorButtons = e.target.innerText;
-    const calcButtons = this.caluculatorButtons;
-    const obj = this.state;
-    this.setState(calculate(obj, calcButtons));
-  }
+  const executeCalculation = (e) => {
+    const caluculatorButtons = e.target.innerText;
+    const calcButtons = caluculatorButtons;
+    const obj = state;
+    setState(calculate(obj, calcButtons));
+  };
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div className="calc-container">
-        <div className="output">{ next || operation || total || 0 }</div>
-        <button type="button" className="" onClick={this.executeCalculation.bind(this)}>AC</button>
-        <button type="button" onClick={this.executeCalculation.bind(this)}>+/-</button>
-        <button on type="button" onClick={this.executeCalculation.bind(this)}>%</button>
-        <button type="button" className="" onClick={this.executeCalculation.bind(this)}>÷</button>
-        <button type="button" onClick={this.executeCalculation.bind(this)}>7</button>
-        <button type="button" onClick={this.executeCalculation.bind(this)}>8</button>
-        <button on type="button" onClick={this.executeCalculation.bind(this)}>9</button>
-        <button type="button" onClick={this.executeCalculation.bind(this)}>x</button>
-        <button type="button" onClick={this.executeCalculation.bind(this)}>4</button>
-        <button type="button" onClick={this.executeCalculation.bind(this)}>5</button>
-        <button on type="button" onClick={this.executeCalculation.bind(this)}>6</button>
-        <button type="button" onClick={this.executeCalculation.bind(this)}>-</button>
-        <button type="button" onClick={this.executeCalculation.bind(this)}>1</button>
-        <button type="button" onClick={this.executeCalculation.bind(this)}>2</button>
-        <button type="button" className="span-t-b" onClick={this.executeCalculation.bind(this)}>3</button>
-        <button type="button" onClick={this.executeCalculation.bind(this)}>+</button>
-        <button type="button" className="zero-span2" onClick={this.executeCalculation.bind(this)}>0</button>
-        <button type="button" onClick={this.executeCalculation.bind(this)}>.</button>
-        <button type="button" onClick={this.executeCalculation.bind(this)}>=</button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="calc-container">
+      <div className="output">{ next || operation || total || 0 }</div>
+      <button type="button" onClick={executeCalculation}>AC</button>
+      <button type="button" onClick={executeCalculation}>+/-</button>
+      <button type="button" onClick={executeCalculation}>%</button>
+      <button type="button" onClick={executeCalculation}>÷</button>
+      <button type="button" onClick={executeCalculation}>7</button>
+      <button type="button" onClick={executeCalculation}>8</button>
+      <button type="button" onClick={executeCalculation}>9</button>
+      <button type="button" onClick={executeCalculation}>x</button>
+      <button type="button" onClick={executeCalculation}>4</button>
+      <button type="button" onClick={executeCalculation}>5</button>
+      <button type="button" onClick={executeCalculation}>6</button>
+      <button type="button" onClick={executeCalculation}>-</button>
+      <button type="button" onClick={executeCalculation}>1</button>
+      <button type="button" onClick={executeCalculation}>2</button>
+      <button type="button" className="span-t-b" onClick={executeCalculation}>3</button>
+      <button type="button" onClick={executeCalculation}>+</button>
+      <button type="button" className="zero-span2" onClick={executeCalculation}>0</button>
+      <button type="button" onClick={executeCalculation}>.</button>
+      <button type="button" onClick={executeCalculation}>=</button>
+    </div>
+  );
+};
 
 export default Calculator;
