3565bcfa39e0f96441426c178cf4fd5e2f452d76
zieeco <zieecosam@gmail.com>
2022-02-11 13:19:12 +0100

    Refactor using hooks

--- src/App.js
+++ src/App.js
@@ -1,20 +1,6 @@
-import React, { Component } from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <Calculator />
-      </div>
-    );
-  }
-}
+const App = () => <div className="container"><Calculator /></div>;
 
 export default App;
